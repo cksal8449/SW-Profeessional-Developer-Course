77105b1f23ec503b102ee42a546351ad40622355
cksal8449 <cksal8449@naver.com>
2023-06-23 14:02:03 +0900

    📝 타이머 만들기

---
+++ 20230623/setinterval/script.js
@@ -0,0 +1,26 @@
+const number = document.querySelector('.number')
+const startBtn = document.getElementById('start')
+const stopBtn = document.getElementById('stop')
+const list = document.getElementById('list')
+let index = 0;
+// 1000 milliseconds => 1 second
+let timerId;
+
+startBtn.addEventListener('click',() => {
+  index = 0;
+timerId = setInterval(()=>{
+  index++
+  number.textContent = index;
+  // console.log(index)
+},100)
+})
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId)
+  const li = document.createElement('li')
+  li.textContent = index
+  list.appendChild(li)
+})
+
+// console.log(timerId)
+
