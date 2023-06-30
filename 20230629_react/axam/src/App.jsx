242a1c2104d06e806abd092d4f837f18751b9cb2
cksal8449 <cksal8449@naver.com>
2023-06-30 16:47:31 +0900

    📝

--- 20230629_react/axam/src/App.jsx
+++ 20230629_react/axam/src/App.jsx
@@ -1,19 +1,25 @@
 import './App.css'
-import Button from './components/Button.jsx'
-import Heading from './components/Heading.jsx'
 import Flex from './components/Flex'
+import Image from './components/Image'
 
+const images = [
+  {id: 1, url:'/a.jpg', title:'pink1'},
+  {id: 2, url:'/b.jpg', title:'pink2'},
+  {id: 3, url:'/c.jpg', title:'pink3'},
+]
 
 function App() { 
-  return (    
-    <> 
-       <Flex>
-      <Heading title="안녕하세요"/>
-      <Button name="1번버튼" primary={true}/>
-      <Button name="2번버튼" secondary/>
-      <Button name="3번버튼" />
-      </Flex> 
-    </>
+  return ( 
+    <>
+    <Flex> 
+    {images.map(image => (
+      <div key={image.id}>
+        <Image url={image.url} title={image.title}/>
+      <h2>{image.title}</h2>
+      </div>
+        ))}
+      </Flex>
+    </>   
   )
 }
 
---
+++ 20230629_react/axam/src/components/Image.jsx
@@ -0,0 +1,11 @@
+const Image = ({url, title}) => {
+  return(
+    <figure
+    className="image-wrapper">
+    <img className="image" src={url} 
+    alt={title}/>
+    </figure>
+  )
+}
+
+export default Image
