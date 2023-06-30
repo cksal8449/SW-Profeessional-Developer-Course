508674ff3498317d7fe45b41f324307fb4e6fc39
cksal8449 <cksal8449@naver.com>
2023-06-30 15:09:48 +0900

    📝

---
+++ 20230629_react/axam/.prettier.rc.js
@@ -0,0 +1,7 @@
+// 파일명
+// .prettier.rc.js
+module.exports ={
+  printWidth: 80,
+  tavWidth: 2,
+  semi:false
+}
--- 20230629_react/axam/src/components/Button.jsx
+++ 20230629_react/axam/src/components/Button.jsx
@@ -7,8 +7,18 @@
 // ---------------------
 // { name, color, size}
 // ---------------------
-export default function Button({name, color, size}){
+import style from "./Button.module.css"
+
+export default function Button({name,primary,secondary}){
+  // 3항 연산자
+  const isPrimary = primary ? style.primary : ''
+  const isSecondary = secondary ? style.secondary : ''
+  // if(primary){
+  //   return style.primary
+  // } else {
+  //   return ''
+  // }
   return(
-    <button style={{color:color,fontSize:size}}>{name}</button>
+    <button className={[style.button, isPrimary, isSecondary].join(" ")}>{name}</button>
   )
 }
