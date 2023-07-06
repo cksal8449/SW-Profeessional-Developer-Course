52e3d2ac1f975cd80ab19d4b3a97e08b542582f1
cksal8449 <cksal8449@naver.com>
2023-07-06 15:37:13 +0900

    ⚡️ Dropdown menu toggle -> 조건문으로 구현

--- 20230705_Github Profile예제 풀이/dropdown.js
+++ 20230705_Github Profile예제 풀이/dropdown.js
@@ -1,18 +1,32 @@
-// dropdown-btn을 클릭했을 때 dropdown-menu한테 active class 추가
-// dropdown-menu가 active class를 가지고 있을 때 클릭하면 active remove
+// dropdown-btn을 클릭 했을 때 dropdown-menu한테 active class 추가
+// dropdown-menu가 active를 가지고 있을 때 클릭하면 active class remove
 // toggle
+let dropdownBtn = document.querySelector('.dropdown-btn');
+let dropdownMenu = document.querySelector('.dropdown-menu');
 
-let dropDownBtn = document.querySelector('.dropdown-btn');
-let dropDownMenu = document.querySelector('.dropdown-menu');
-// console.log(dropDownBtn)
-
-// console.log('13131')
-
-
-//function(e){
-//e.preventDefault();
-// a태그가 있어도 이벤트가 렉없이 발생하게 함 
-dropDownBtn.addEventListener('click', function(e){
+dropdownBtn.addEventListener('click', function(e){
   e.preventDefault();
-  dropDownMenu.classList.toggle('active');
+  // dropdownMenu.classList.toggle('active');
+
+  // 조건문으로 toggle 기능 구현
+  // (소괄호)안에 있는 조건이 True(참)일시 중괄호 안에 있는 코드 실행
+  // 조건이 False일 경우, else 구문 실행
+
+  // active로 현재 버튼의 활성화 상태를 확인
+  // 활성화 상태라면 True니까 if 중괄호 코드 실행 
+  // 활성화 상태가 아니라면 False니까 else 구문 실행
+  if(this.active) {
+    // active 상태가 활성화 되어 있다면 -> .active remove
+    dropdownMenu.classList.remove('active');
+  } else {
+    // active가 비 활성화 되어 있다면 -> .active add
+    dropdownMenu.classList.add('active');
+  }
+
+  // NOT 연산자 !
+  // 버튼이 비활성화 상태라면 활성 상태로
+  // 활성 상태 -> 비활성화 상태
+  // this.active : 현재 버튼의 활성 상태
+  // !this.active : 현재 버튼 활성 상태의 반대 값
+  this.active = !this.active
 })
