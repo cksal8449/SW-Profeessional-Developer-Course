dda08e32644be08c7bd512d60e1e4e4c1615894a
cksal8449 <cksal8449@naver.com>
2023-07-10 15:34:15 +0900

    ✨ 조건문으로 prev, next slide 구현

---
+++ 20230706_slid/slide3 예제풀이/script.js
@@ -0,0 +1,24 @@
+let slideList = document.querySelector('.slide-list');
+// 여러개니깐 all로 가져와야함
+let slideItems = document.querySelectorAll('.slide-list li');
+// console.log(slideItems.length)
+let prevBtn = document.querySelector('.prev');
+let nextBtn = document.querySelector('.next');
+
+let cuurentSlide = 0;
+// console.log(cuurentSlide)
+
+nextBtn.addEventListener('click', function(){
+  // slideItems 배열 length의 -1보다 값이 작을 때
+  if(cuurentSlide < slideItems.length -1){
+    cuurentSlide++;
+    slideList.style.transform = `tanslateX(-${cuurentSlide* 100}vw)`
+  }
+})
+
+prevBtn.addEventListener('click', function(){
+  if(cuurentSlide > 0){
+    cuurentSlide--;
+    slideList.style.trensform = 'tanslateX(-'+cuurentSlide*100+'vw)'
+  }
+})
