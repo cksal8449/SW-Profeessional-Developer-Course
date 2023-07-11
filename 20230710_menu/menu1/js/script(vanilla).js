be642631d19c890262876805d15b2173a0a3ba9d
cksal8449 <cksal8449@naver.com>
2023-07-11 15:31:12 +0900

    ✨ menu1 Vanilla Js로 구현

---
+++ 20230710_menu/menu1/js/script(vanilla).js
@@ -0,0 +1,19 @@
+const menuItems = document.querySelectorAll('.gnb > li');
+
+// item이라는 매개 변수에 이벤트를 추가
+menuItems.forEach(function(item){
+  // document가 아닌 item에서 찾아 줘야 전체 가져올 수 있음
+const subMenu = item.querySelector('.sub');
+
+item.addEventListener('mouseover', function(){
+  subMenu.classList.add('active')
+  // on이라는 클래스 추가
+  this.querySelector('a').classList.add('on');
+})
+
+// mousleave
+item.addEventListener('mouseover', function(){
+  subMenu.classList.remove('active');
+  this.querySelector('a').classList.remove('on');
+})
+})
