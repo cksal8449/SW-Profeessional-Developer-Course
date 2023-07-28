40cfb4d17c9080bdfb984aa0d799a29e72166c93
cksal8449 <cksal8449@naver.com>
2023-07-28 16:43:42 +0900

    00

---
+++ 20230726_네이버회원가입/js/약관동의.js
@@ -0,0 +1,7 @@
+let label = document.querySelector('label')
+let chkImg = document.getElementsByClassName('.checkbox-img')
+let chkBox = document.getElementsByClassName('.checkbox')
+
+function clickCheckbox(e) {
+  
+}
