76ba96b43b9108cc4032baefdf45570d7d5c0974
cksal8449 <cksal8449@naver.com>
2023-08-07 13:51:11 +0900

    ✨ Vanilla JS로 비밀번호 조건문 구현

--- 20230726_네이버회원가입예제풀이/join.js
+++ 20230726_네이버회원가입예제풀이/join.js
@@ -33,3 +33,31 @@ document.querySelector('.userid input').addEventListener('focusout', function(){
   }
 })
 
+// 비밀번호 
+document.querySelector('.userpw input').addEventListener('focusout', function(){
+  let userPw = this.value;
+  let pwExp = /^[A-Za-z0-9`~!@#\$%\^&\*\(\)\{\}\[\]\-_=\+\\|;:'"<>,\./\?]{8,20}$/
+
+  let pwWarn = document.querySelector('.userpw .warn');
+  let pwText = document.querySelector('.userpw .inputbox p');
+  let pwImg = document.querySelector('.userpw .inputbox img');
+
+  if(userPw.length == 0) {
+    pwWarn.innerHTML = essenInfo;
+    // pwText 내용 비우기
+    pwText.innerHTML = '';
+    pwImg.src ='./images/m_icon_pw_step_01.png';
+  } else if(!pwExp.test(userPw)) {
+    // 정규식에 맞지 않을 때 = 조건이 참
+    pwWarn.innerHTML = '<span class="text-red">8~20자 영문 대 소문자, 숫자, 특수문자를 사용하세요.</span>'
+    pwText.innerHTML = '<span class="text-red">사용불가</span>'
+    pwImg.src ='./images/m_icon_pw_step_10.png';
+  } else {
+    // 위 조건에 둘 다 해당하지 않고 값을 잘 입력 했을 때
+    pwveri = true;
+    pwWarn.innerHTML = '';
+    pwText.innerHTML = '<span class="text-green">안전</span>'
+    pwImg.src ='./images/m_icon_pw_step_04.png';
+  }
+})
+
