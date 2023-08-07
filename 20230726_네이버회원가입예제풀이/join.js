735678e1e22f847147bc2b4ba997cceaebd63181
cksal8449 <cksal8449@naver.com>
2023-08-07 13:15:22 +0900

    ✨ 필수항목 변수에 false, 선택항목 변수에 true값

--- 20230726_네이버회원가입예제풀이/join.js
+++ 20230726_네이버회원가입예제풀이/join.js
@@ -9,3 +9,7 @@ document.querySelectorAll('input').forEach(function(input){
     this.parentNode.classList.remove('border-act');
   })
 })
+
+let idveri = pwveri = pwchkveri = nameveri = bitrveri = genderveri = phonveri = addressveri = false;
+let mailveri = true;
+
