cacc2fdd8d674b441f012b80a590a72f4ee87da2
cksal8449 <cksal8449@naver.com>
2023-08-25 15:39:28 +0900

    🚧 구현중

---
+++ 20230825_fitness-flex/script.js
@@ -0,0 +1,21 @@
+let exercise = [
+  {
+    title : '크로스핏',
+    explanation : 'Strength does not come from the physical capacity. It comes from an indomitable will.',
+    more : 'Know more',
+    img : './images/img1.png',
+    color : '--ex1-bg'
+  },
+  {
+    title : '웨이트 훈련',
+    explanation : 'Strength does not come from the physical capacity. It comes from an indomitable will.',
+    more : 'Know more',
+    img : './images/img2.png'
+  },  
+  {
+    title : '요가',
+    explanation : 'Strength does not come from the physical capacity. It comes from an indomitable will.',
+    more : 'Know more',
+    img : './images/img3.png'
+  },
+]
