c12e6c9070a216b9dd52a7b41633bccecac0c590
cksal8449 <cksal8449@naver.com>
2023-09-04 16:40:28 +0900

    ✨ /edit/:id 경로로 접속시 해당 아이디 값 데이터 eidt.ejs에 바인딩

--- express/index.js
+++ express/index.js
@@ -1,4 +1,11 @@
-/// Node.js : Javascript runtime
+// 서버 : 요청한 정보를 보내주는 프로그램
+// HTTP 요청 방식 네가지
+// 1. GET(읽기)
+// 2. POST(쓰기)
+// 3. PUT(수정)
+// 4. DELETE(삭제)
+
+// Node.js : Javascript runtime
 // Javascript는 프로그래밍적 연산을 하기 위한 언어가 아니고, 
 // HTML을 조작하기 위해 만들어진 언어다.
 // Javascript는 브라우저가 해석한다. (크롬, 사파리, 엣지, 파이어폭스 등)
@@ -24,8 +31,7 @@
 // npm : 라이브러리를 설치하기 위한 도구
 // node_modules : 라이브러리 사용할 때 필요한 것들이 담긴 폴더
 
-
-// express로 서버 만드는 문법 
+// express로 서버 만드는 문법
 const express = require('express');
 const app = express();
 
@@ -40,17 +46,9 @@ const app = express();
 // 내 컴퓨터에서 7000번 포트로 진입 했을 때,
 // 콜백함수 안에 있는 코드 실행
 // localhost:7000(port number)
-
 app.listen(7000, function(){
   console.log('7000번 포트')
 })
-
-// 서버 : 요청한 정보를 보내주는 프로그램
-// HTTP 요청 방식 네가지
-// 1. GET (읽기)
-// 2. POST (쓰기)
-// 3. PUT (수정)
-// 4. DELETE (삭제)
 
 
 // 폴더 내 모든 정적 파일 제공(js, css, image, fonts)
@@ -61,7 +59,7 @@ app.use(express.static(__dirname))
 // localhost:7000으로 접속하면 send, sendFile() 안 내용을 보여준다.
 // app.get('경로', function(){})
 // requests(요청), response(응답)
-// slash / 는 메인경로(localhost:7000)
+// slash / 는 메인 경로 (localhost:7000)
 app.get('/', function(requests, response){
   response.sendFile(__dirname + '/index.html');
 })
@@ -78,6 +76,7 @@ app.get('/test', function(requests, response){
 // npm install -g nodemon
 // node index.js => nodemon index.js
 
+
 // powershell 보안 에러
 // 에러 원인 : Restricted일 때 허가된 script외에 막아버리기 때문에 에러가 발생한다.
 
@@ -86,20 +85,21 @@ app.get('/test', function(requests, response){
 // set-executionpolicy unrestricted => enter
 // y => enter
 
+
 // localhost:7000/login 으로 접속시 보여줄 화면 => login.html
 app.get('/login', function(requests, response){
-  response.sendFile(__dirname + '/login.html');
+  response.sendFile(__dirname + '/login.html')
 })
 
 // localhost:7000/map 으로 접속시 보여줄 화면 => map.html
 // map.html : 카카오 지도 OPEN API
 app.get('/map', function(requests, response){
-  response.sendFile(__dirname + '/map.html');
+  response.sendFile(__dirname + '/map.html')
 })
 
 
-// POST 
-// body-parser: 요청 데이터 해석을 도와주는 라이브러리
+// POST
+// body-parser : 요청 데이터 해석을 도와주는 라이브러리
 // body-parser 라이브러리 설치
 // npm install body-perser
 const bodyParser = require('body-parser');
@@ -117,58 +117,26 @@ app.use(bodyParser.urlencoded({extended : true}));
 // })
 
 // 서버한테 정보를 보내주는 코드
-// 서버에 보낸 정보를 영구 저장하려면 DB(Data Base)에 저장
+// 서버에 보낸 정보를 영구 저장 하려면 DB(Data Base)에 저장
+
 
 // url 이름짓기
 // 1. URL 명사로 작성 추천 '/명사'
 // 2. 하위 문서를 나타낼 때 / slash (하위폴더 나누듯이 사용)
-// 3. 파일 확장자 사용 X (.html. .css 등)
-// 4. 띄어쓰기 대신 (-) 사용 
+// 3. 파일 확장자 사용 X (.html, .css 등)
+// 4. 띄어쓰기 대신 (-) 사용
 // 5. 자료 하나당 하나의 URL 사용
 // 6. URL을 봤을 때 어떤 페이지인지 알 수 있어야 한다.
 
-
-// MonogoDB
+// MongoDB
 // npm install mongodb@3.6.4
-// const MongoClient = require('mongodb').MongoClient;
-
-// // 데이터를 저장할 변수 하나 선언
-// let db;
-
-
-
-// Database access에서 만든 아이디 : 비밀번호
-// MongoClient.connect('mongodb+srv://admin:tkdlws4102@dara.r4t7mzn.mongodb.net/?retryWrites=true&w=majority', function(error, client){
-//   // 커넥션 에러의 99.9%가 url 오타
-//   if(error) {
-//     return console.log(error)
-//   }
-
-//   db = client.db('data');
-//   app.listen('7070', function(){
-//     console.log('seccess')
-//   })
-
-// })
-
-// app.post('/add', function(requests, response){
-//   console.log(requests.body)
-//   response.send('전송완료!')
-
-//   // db.collection('post').insertOen({아이디 : requests.body.id, 비밀번호 : requests.body.pw}, function(error, result){
-//   //   console.log('db에 저장완료!')
-//   // })
-
-// })
-
-
 const MongoClient = require('mongodb').MongoClient;
 
 // 데이터를 저장할 변수 하나 선언
 let db;
 
 // Database access에서 만든 아이디 : 비밀번호
-MongoClient.connect('mongodb+srv://admin:1234@cluster0.5o4ysq5.mongodb.net/?retryWrites=true&w=majority', function(error, client){
+MongoClient.connect('mongodb+srv://admin:wmfdlekt12@data.pvgodwt.mongodb.net/?retryWrites=true&w=majority', function(error, client){
   // 커넥션 에러의 99.9%가 url 오타
   if(error) {
     return console.log(error)
@@ -179,6 +147,7 @@ MongoClient.connect('mongodb+srv://admin:1234@cluster0.5o4ysq5.mongodb.net/?retr
     console.log('dsgdsgdsg')
   })
 })
+
 
 // form에서 /add 경로로 post 요청을 하면, 
 // DB에서 total collection을 찾아서
@@ -191,8 +160,6 @@ app.post('/add', function(requests, response){
   console.log(requests.body)
   response.send('전송 완료!')
 
-
-
   // DB에서 total collection 총 데이터 수 꺼내오기.
   // 데이터를 전부 찾고 싶다면 find(), 하나만 찾고 싶으면 findOne()
   // name이 totalData인 데이터를 찾아달라는 쿼리문
@@ -203,7 +170,7 @@ app.post('/add', function(requests, response){
     db.collection('post').insertOne({_id : totalDataLength + 1 ,아이디 : requests.body.id, 비밀번호 : requests.body.pw}, function(error, result){
       console.log('db에 저장완료!')
     })
-
+  
     // 새로운 데이터가 저장 됐을 때 total collection에 있는 totalData + 1
     // .updateOne({변경 할 데이터}, {$inc : {수정값}})
     // update operator(연산자) $set, $inc(증가) 등 여러가지 
@@ -230,54 +197,52 @@ app.get('/add', function(requests, response){
   })
 })
 
+
 app.delete('/delete', function(requests, response){
   console.log(requests.body._id)
   requests.body._id = parseInt(requests.body._id)
-
-
-  db.collection('post').deleteOne({_id : requests.body._id}, function(error, result){
+  
+  db.collection('post').deleteOne({_id : requests.body._id}, function(error, reulst){
     if(error) {
-      console.log(erorr)
+      console.log(error)
     }
-    console.log('삭제완료!')
+    console.log('삭제완료!!')
   })
-  // 서버에서 응답코드로 요청의 상태를 표시 할 수 있다.
+
+  // 서버에서 응답코드로 요청의 상태를 표시할 수 있다.
   // 2xx => 요청 성공
   // 4xx => 고객 문제로 요청 실패
-  // 5xx -> 서버 문제로 요청 실패
+  // 5xx => 서버 문제로 요청 실패
   response.status(200).send({message : '성공적'})
 })
+
 
 // url에 데이터가 가지고 있는 _id 값을 파라미터로 받는다.
 // 각 페이지에 보여줄 내용이 다르기 때문에 
 // 내용에 따라서 경로 변경 (_id 값 사용)
-// /info/1 /info/2 
-// url 파라미터 == 함수 파라미터 
+// /info/1  /info/2
+// url 파라미터 == 함수 파라미터
 // '/info/:id' : 콜론 뒤에 아무 문자나 입력 했을 때
 app.get('/info/:id', function(requests, response){
-  // params.id : url 파라미터 중 id 값,
+  // params.id : url 파라미터 중 id값
   // 'post' collection에서 params.id 값에 해당하는 데이터 찾아오기
-  // 데이터 찾을 때 requests.params.id String  => Int 형변환
-  // 'post' collection에 _id값이 int기 때문!
-    // console.log(typeof(requests.params.id))
-    db.collection('post').findOne({_id : parseInt(requests.params.id)}, function(error, result){
-      console.log(result)
-      response.render('info.ejs', {data : result})
-    })
- })
-
-
-// /edit.ejs 경로로 접속시 edit.ejs 내용 보여주기
-// /edit/:id 
-// params.id로 받은 _id 값  db.collection post에서 가져와서
-// edit.ejs input(id,pw) value 값으로 바인딩
- app.get('/edit/:id', function(requests, response){
-    db.collection('post').findOne({_id : parseInt(requests.params.id)}, function(error, result){
-      console.log(result)
-      response.render('edit.ejs', {data : result})
-    })
+  // 데이터 찾을 때 requests.params.id String -> Int로 형변환
+  // 'post' collection에 _id 값이 int기 때문!
+  // console.log(typeof(requests.params.id))
+  db.collection('post').findOne({_id : parseInt(requests.params.id)}, function(error, result){
+    console.log(result)
+    response.render('info.ejs', {data : result})
   })
+})
 
 
-
-
+// /edit 경로로 접속시 edit.ejs 내용 보여주기
+// /edit/:id
+// params.id로 받은 _id 값 db collection post에서 가져와서
+// edit.ejs input(id,pw) value 값으로 바인딩
+app.get('/edit/:id', function(requests, response){
+  db.collection('post').findOne({_id : parseInt(requests.params.id)}, function(error, result){
+    console.log(result)
+    response.render('edit.ejs', {data : result})
+  })
+})
