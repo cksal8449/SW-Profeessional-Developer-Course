ad00100ae06739cc4c4167af61f3edfa68f94b65
cksal8449 <cksal8449@naver.com>
2023-09-07 16:49:00 +0900

    🚧  미세먼지 농도에 따른 조건문 구현중

--- 20230907_미세먼지API/script.js
+++ 20230907_미세먼지API/script.js
@@ -2,7 +2,7 @@ var xhr = new XMLHttpRequest();
 var url = 'http://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureSidoLIst'; /*URL*/
 var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'rOGI%2BIvn6bqhwghISJpom2Dz1zr0tbNdR%2Blm8fl5xrLwUFt9EGQHinJBCAQhO4c3yub9o5bhWPy9AxyTKTen%2BQ%3D%3D'; /*Service Key*/
 queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('json'); /* 응답 데이터 타입 설정 */
-queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /* 한 페이지 결과 수 */
+queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /* 한 페이지 결과 수 */
 queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* 페이지 번호 설정 */
 queryParams += '&' + encodeURIComponent('sidoName') + '=' + encodeURIComponent('경북'); /* 조회 할 데이터 시도 이름 설정*/
 queryParams += '&' + encodeURIComponent('searchCondition') + '=' + encodeURIComponent('DAILY'); /* 데이터 기간 */
@@ -54,3 +54,14 @@ function updateData(){
 }
 
 updateData();
+
+if (item.pm10Value <= 30) {
+  item.pm10Value.innerHTML = 좋음
+  console.log=('좋음')
+} else if (item.pm10Value <= 80) {
+  console.log=('보통')
+}else if (item.pm10Value <= 150) {
+  console.log=('나쁨')
+}else {
+  console.log=('매우나쁨')
+};
