993f4cb6312216c9774d2d3b5d0cbd072ff07516
cksal8449 <cksal8449@naver.com>
2023-09-20 13:26:52 +0900

    ✨ setBg 상태변경 함수로 배경색 변경

--- React/react-basic/src/component/State.jsx
+++ React/react-basic/src/component/State.jsx
@@ -24,7 +24,9 @@ const State = () => {
   let changeBg = () => {
     // 삼항 연산자로 현재 배경색이 white라면
     // 다른색으로 바꿔주고, 아니라면 white로 바꿔라
-  //  <div style={{bagroundColor= {bg == white} ? 'blue' : {bg != white} ? 'white'}}></div>
+    //  <div style={{bagroundColor= {bg == white} ? 'blue' : {bg != white} ? 'white'}}></div>
+    let newBg = bg == 'white' ? 'green' : 'white';
+    setBg(newBg);
   }
 
   // count : 해당 상태값을 의미하는 변수
@@ -32,7 +34,8 @@ const State = () => {
   // 이 함수를 호출할 때 변경된 값을 인수로 전달한다.
   // (0) : 초기 상태 값 / 컴포넌트가 처음 랜더링 될 때 사용되는 값
   return (
-    <div>
+    <div style={{backgroundColor : bg}}>
+    <button onClick={changeBg}>배경색 변경</button>
       <h1>{count}</h1>
       <button onClick={() => setCount(count + 1)}>Click+</button>
       <button onClick={() => setCount(count - 1)}>Click-</button>
@@ -56,9 +59,6 @@ const State = () => {
         nameCopy[0] = '강아지'; 
         setName(nameCopy)
       }}>이름변경</button>
-
-      <h2>배경색</h2>
-      <button>배경색 변경</button>
 
     </div>
   );
