3761ae9123734f4f6bdaaa0e09540ece80db3337
cksal8449 <cksal8449@naver.com>
2023-09-21 16:09:27 +0900

    ✨ Back component  구현 완료

---
+++ React/react-basic/src/component/Main/Back.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import back from './/Back.css'
+
+// 부모 컴포넌트
+const Back = () => {
+  return (
+    <div>
+      <Box layout='background' name='front-txt'/> 
+      <Box layout='top' name='second-txt'/>
+    </div>
+  );
+};
+
+function Box(props){
+  // 조건부 랜더링
+  let showBtn = props.name == 'front-txt' ? true : false;
+  return (
+    <>
+      <section className={props.layout}>
+        <div className={props.name}>
+          <h2>Lorem ipsum dolor sit amet.</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit nesciunt blanditiis cumque quibusdam molestias deserunt sed tempora minima possimus odit!</p>
+          {
+            // html 조건부로 보여줄 떄
+            // showBtn이 true일 때만 button을 랜더링 해준다.
+            // &&(and) 우항, 좌항 둘 다 true일 경우 랜더링
+            // 좌항 false일 경우 바로 false 반환 (랜더링 x)
+
+            // 이벤트 핸들러에 함수가 아닌 표현식이나 값을 바로 전달 했을 경우
+            // 컴포넌트가 재랜더링 될 때 마다 해당 값이 실행된다.
+            // onClick 됐을 때 실행되게 하려면 이벤트핸들러에 값을 함수로 전달해야한다.
+            showBtn && <button onClick={()=>alert('안녕')} button className='front-btn' type='button'>버튼</button>
+          }
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Back;
