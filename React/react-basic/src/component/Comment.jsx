37ea4164b69650f95b99426fc4de8dd4a26216e4
cksal8449 <cksal8449@naver.com>
2023-09-22 14:41:20 +0900

    💄 Comment component styling

---
+++ React/react-basic/src/component/Comment.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+let styles = {
+  wrapper : {
+    display : 'flex',
+    justifyContent : 'space-between',
+    alignItems: 'center',
+    border : '2px solid #ccc',
+    borderRadius: '16px',
+    padding: '0 20px',
+    margin: '20px'
+  },
+  img : {
+    width: '50px',
+    height: '50px',
+    borderRadius: '50%',
+  },
+  contentBox : {
+    flexGrow: '1',
+    marginLeft: '16px',
+  },
+}
+
+const Comment = () => {
+  return (
+    <div style={styles.wrapper}>
+      <div>
+        <img style={styles.img} src="https://www.sragenkab.go.id/assets/images/demo/user-7.jpg" alt="user-img" />
+      </div>
+
+      <div style={styles.contentBox}>
+        <h3>User name</h3>
+        <p>Content</p>
+      </div>
+
+      <button type='butten'>X</button>
+    </div>
+  );
+};
+
+export default Comment;
