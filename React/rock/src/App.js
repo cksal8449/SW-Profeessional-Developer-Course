1c1001cce131181aa6086c8d13b9854b834aa023
cksal8449 <cksal8449@naver.com>
2023-09-25 16:10:19 +0900

    🚧 컴퓨터 랜덤값 선택 기능 구현

--- React/rock/src/App.js
+++ React/rock/src/App.js
@@ -22,9 +22,39 @@ function App() {
   let [comSelect, setComSelect] = useState(null);
   let [result, setResult]= useState(null);
 
-  let play = () => {
+  // 유저랑 컴퓨터가 선택한 값 업데이트
+  // let play = (userChoice) => {
+  //   setUserSelect(choice[userChoice])
+  //   let computerChoice = randomChoice()
+  //   setComputerSelect(computerChoice);
+  //   judgement(choice[userChoice], computerChoice);
+  // }
 
+  // 컴퓨터 랜덤 값 선택
+  let randeomChoice = () => {
+    // choice object 키값을 배열로 만들어주는 함수 사용
+    // keys() : 객체의 key 값을 뽑아서 Array로 만들어주는 함수
+    let itemArr = Object.keys(choice);
+
+    // 0부터 2사이에 있는 랜덤 값
+    // Math floor 소수점 이하를 버리는 함수
+    let randomItem = Math.floor(Math.random() * itemArr.length);
+    let final = itemArr[randomItem]
+    // 해당 함수가 호출 됐을 때 반환할 값
+    return choice[final];
   }
+  randeomChoice()
+
+  // 조건문으로 유저, 컴퓨터 중 누가 이겼는지 판단
+  // let judgement = () => {
+  //   if(user.name === computer.name) {
+  //     return 'tie'
+  //   } else if (user.name === 'Rock')
+  //    return computer.name === 'Scissirs' ? 'win' : 'lose';
+  //    else if (user.name === 'Paper')
+  //    return computer.name === 'Rock' ? 'win' : 'los';
+  // } 
+
 
   return (
     <div className="App">
@@ -33,9 +63,9 @@ function App() {
       <Box title="Computer"/>
       </div>
       <div className="btn-list">
-      <button type='button'>가위</button>
-      <button type='button'>바위</button>
-      <button type='button'>보</button>
+        <button type='button'>가위</button>
+        <button type='button'>바위</button>
+        <button type='button'>보</button>
       </div>
     </div>
   );
