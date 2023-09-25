b1eeeb4299bfe0770ee0123593301f064070b786
cksal8449 <cksal8449@naver.com>
2023-09-25 14:18:01 +0900

    ✨ 가위바위보 마크업 완료

---
+++ React/rock/src/App.js
@@ -0,0 +1,19 @@
+import Box from './component/Box';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='box-list'>
+      <Box title="You"/>
+      <Box title="Computer"/>
+      </div>
+      <div className="btn-list">
+      <button type='button'>가위</button>
+      <button type='button'>바위</button>
+      <button type='button'>보</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
