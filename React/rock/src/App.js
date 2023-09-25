b30527be7861dce66e30dabf4c29e485ffa696ed
cksal8449 <cksal8449@naver.com>
2023-09-25 16:47:08 +0900

    🚧 유저, 컴퓨터 선택값으로 승패 여부 판단하는 함수 구현

--- React/rock/src/App.js
+++ React/rock/src/App.js
@@ -1,71 +1,79 @@
-import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 import Box from './component/Box';
-
+import { useState } from 'react';
 
 let choice = {
   rock : {
-    name: 'Rock',
-    img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRslBxrOKMaRohjaOBJ_qEpkIE_4D-zEN49mg&usqp=CAU'
+    name : 'Rock',
+    img : 'https://nationaltoday.com/wp-content/uploads/2021/08/National-Pet-Rock-Day.jpg'
   },
   paper : {
-    name : 'Paper',
-    img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8JfmPQiuDsvL7wyeQiqqXm313FsXdhvmqyg&usqp=CAU'
+    name : 'Paper', 
+    img : 'https://dictionary.cambridge.org/ko/images/thumb/paper_noun_002_26402.jpg?version=5.0.305'
   },
   scissors : {
     name : 'Scissors',
-    img:'https://e7.pngegg.com/pngimages/162/334/png-clipart-pokemon-x-and-y-pokemon-go-pokemon-battle-revolution-scyther-pokemon-go-leaf-vertebrate.png'
+    img : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQtZOIPMrEps54-OsxSI1J50Pr5znQx6lDaA&usqp=CAU'
   }
 }
 
 function App() {
   let [userSelect, setUserSelect] = useState(null);
   let [comSelect, setComSelect] = useState(null);
-  let [result, setResult]= useState(null);
+  let [result, setResult] = useState('');
 
   // 유저랑 컴퓨터가 선택한 값 업데이트
-  // let play = (userChoice) => {
-  //   setUserSelect(choice[userChoice])
-  //   let computerChoice = randomChoice()
-  //   setComputerSelect(computerChoice);
-  //   judgement(choice[userChoice], computerChoice);
-  // }
+  let play = (userChoice) => {
+    let userChoice = choice[userChoice];
+    setUserSelect(userChoice)
+
+    let comChoice = randomChoice();
+    setComSelect(comChoice);
+
+    setResult(judgment(userChoice, comChoice))
+  }
 
   // 컴퓨터 랜덤 값 선택
-  let randeomChoice = () => {
+  let randomChoice = () => {
     // choice object 키값을 배열로 만들어주는 함수 사용
     // keys() : 객체의 key 값을 뽑아서 Array로 만들어주는 함수
     let itemArr = Object.keys(choice);
-
+    
     // 0부터 2사이에 있는 랜덤 값
-    // Math floor 소수점 이하를 버리는 함수
-    let randomItem = Math.floor(Math.random() * itemArr.length);
+    // Math.floor 소수점 이하를 버리는 함수
+    let randomItem = Math.floor(Math.random() * itemArr.length)
     let final = itemArr[randomItem]
+
     // 해당 함수가 호출 됐을 때 반환할 값
-    return choice[final];
+    return choice[final]
   }
-  randeomChoice()
+  randomChoice()
 
   // 조건문으로 유저, 컴퓨터 중 누가 이겼는지 판단
-  // let judgement = () => {
-  //   if(user.name === computer.name) {
-  //     return 'tie'
-  //   } else if (user.name === 'Rock')
-  //    return computer.name === 'Scissirs' ? 'win' : 'lose';
-  //    else if (user.name === 'Paper')
-  //    return computer.name === 'Rock' ? 'win' : 'los';
-  // } 
-
+  let judgment = (user, computer) => {
+    // user가 이겼는지, computer 이겼는지
+    // 어떤 값을 이용해서 판단할건지 결정
+    // 사용자가 rock 컴퓨터가 scissors 사용자 win, com lose
+    // 사용자가 선택한 값, 컴퓨터가 선택한 값 비교
+    if(user.name == computer.name) {
+      return 'tie'
+    } else if(user.name == 'Rock') return computer.name == 'Scissors' ? 'win' : 'lose'
+    else if(user.name == 'Paper') return computer.name == 'Rock' ? 'win' : 'lose'
+    else if(user.name == 'Scissors') return computer.name == 'Paper' ? 'win' : 'lose'
+  }
 
   return (
     <div className="App">
       <div className='box-list'>
-      <Box title="You"/>
-      <Box title="Computer"/>
+        <Box title="You"/>
+        <Box title="Computer"/>
       </div>
+
       <div className="btn-list">
-        <button type='button'>가위</button>
-        <button type='button'>바위</button>
-        <button type='button'>보</button>
+        <button onClick={() => play('scissors')} type='button'>가위</button>
+        <button onClick={() => play('rock')} type='button'>바위</button>
+        <button onClick={() => play('paper')} type='button'>보</button>
       </div>
     </div>
   );
