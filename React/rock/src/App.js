21405e5418691577b069178eb58f4ff9d7dbe078
cksal8449 <cksal8449@naver.com>
2023-09-25 16:49:29 +0900

    🐛 play 함수 매개변수 값이랑 겹치는 userChoice 변수 제거

--- React/rock/src/App.js
+++ React/rock/src/App.js
@@ -25,8 +25,7 @@ function App() {
 
   // 유저랑 컴퓨터가 선택한 값 업데이트
   let play = (userChoice) => {
-    let userChoice = choice[userChoice];
-    setUserSelect(userChoice)
+    setUserSelect(choice[userChoice])
 
     let comChoice = randomChoice();
     setComSelect(comChoice);
