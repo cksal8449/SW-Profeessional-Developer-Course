5262cfcdf23b3c49df3940320148322b7f3c0097
cksal8449 <cksal8449@naver.com>
2023-09-25 14:43:10 +0900

    🚧 유저 선택, 컴푸터 선택, 결과 state

--- React/rock/src/App.js
+++ React/rock/src/App.js
@@ -1,6 +1,31 @@
+import { useState } from 'react';
 import Box from './component/Box';
 
+
+let choice = {
+  rock : {
+    name: 'Rock',
+    img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRslBxrOKMaRohjaOBJ_qEpkIE_4D-zEN49mg&usqp=CAU'
+  },
+  paper : {
+    name : 'Paper',
+    img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8JfmPQiuDsvL7wyeQiqqXm313FsXdhvmqyg&usqp=CAU'
+  },
+  scissors : {
+    name : 'Scissors',
+    img:'https://e7.pngegg.com/pngimages/162/334/png-clipart-pokemon-x-and-y-pokemon-go-pokemon-battle-revolution-scyther-pokemon-go-leaf-vertebrate.png'
+  }
+}
+
 function App() {
+  let [userSelect, setUserSelect] = useState(null);
+  let [comSelect, setComSelect] = useState(null);
+  let [result, setResult]= useState(null);
+
+  let play = () => {
+
+  }
+
   return (
     <div className="App">
       <div className='box-list'>
