4aa8b657a9202218a126ea3156c75bde59926f32
cksal8449 <cksal8449@naver.com>
2023-09-26 13:24:24 +0900

    🚧 user & computer 선택값 바인딩

--- React/rock/src/component/Box.jsx
+++ React/rock/src/component/Box.jsx
@@ -5,7 +5,11 @@ const Box = (props) => {
   return (
     <div className="box-item">
       <h1>{props.title}</h1>
-      <h2>Rock</h2>
+      {/* 
+      null error 해결 방법 => 조건부 렌더링 
+      null이 아닐떄만 값이 렌더링 되도록
+      */}
+      <h2>{props.select && props.select.name}</h2>
       <img className='item-img' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRslBxrOKMaRohjaOBJ_qEpkIE_4D-zEN49mg&usqp=CAU" alt="바위 이미지"/>
       <h2>{props.result}tie</h2>
     </div>
